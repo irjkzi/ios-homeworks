4c2b9c9485081ab4e11ddd076c671b8c2241ba69
irjkzi <irjkzi@gmail.com>
2024-11-17 10:27:04 +0400

    Цвет фона профиля изменен на серый

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.systemMint
+        view.backgroundColor = .lightGray
         title = "Profile"
     }
 }
